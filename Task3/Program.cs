eaf10296a31d8227bb6ec091d0652c2618ed2287
NadiaBrin <nadiabrin@gmail.com>
2022-09-16 23:03:42 +0300

    В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+
+void FillMatrix( int[, ,] matrix) 
+{
+    for ( int  i = 0; i < matrix.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for ( int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10,99); 
+
+            }
+        }
+    }    
+}
+
+
+void PrintMatrix(int [,,] matrix) 
+{
+    for ( int  i = 0; i < matrix.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matrix.GetLength(1) ; j++)
+        {
+            for ( int k = 0; k < matrix.GetLength(2); k++)
+            {
+             Console.WriteLine ($"элемент с индексом [{i},{j},{k}] - {matrix[i,j,k]}  ");
+            }
+        Console.WriteLine();
+        }
+    }
+}
+
+int[, ,] matrix = new int[3, 3, 3];
+FillMatrix(matrix);
+PrintMatrix(matrix);
